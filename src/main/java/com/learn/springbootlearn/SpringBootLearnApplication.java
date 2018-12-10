51059947bb1d4276470da02fb8ab584257a7d2a6
wch <527622102@qq.com>
2018-12-10 12:55:17 +0800

    init

---
+++ src/main/java/com/learn/springbootlearn/SpringBootLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springbootlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SpringBootLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearnApplication.class, args);
+    }
+}
