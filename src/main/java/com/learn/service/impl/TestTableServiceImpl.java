ce591aece0c8448437b0dd71f9a26e3d346f8a4a
wch <527622102@qq.com>
2018-12-18 11:40:44 +0800

    add model

---
+++ src/main/java/com/learn/service/impl/TestTableServiceImpl.java
@@ -0,0 +1,8 @@
+package com.learn.service.impl;
+
+import com.learn.service.TestTableService;
+import org.springframework.stereotype.Service;
+
+@Service("testTableService")
+public class TestTableServiceImpl implements TestTableService {
+}
